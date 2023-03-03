8b0aabef20fac529d9d11bb44e7f9a6bc817723c
Aryan Khanna <aryan.khanna@chicmic.co.in>
2023-03-03 10:22:48 +0530

    Enum

---
+++ assets/scripts/antType.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+import { ANT_TYPES } from './constants';
+@ccclass('antType')
+export class antType extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
---
+++ assets/scripts/constants.ts
@@ -0,0 +1,8 @@
+export enum ANT_TYPES {
+  JUST_ANT = 0,
+  LITTLE_STRONG_ANT = 1,
+  FAST_ANT = 2,
+  BOMB_ANT = 3,
+  DEFENCE_ANT = 4,
+  OFFENCE_ANT = 5,
+}
