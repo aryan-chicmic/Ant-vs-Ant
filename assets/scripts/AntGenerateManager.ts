4134c68a56105c039f84f3a7ea04963f85ecc02d
SamridhG <samridh1998@gmail.com>
2023-03-05 16:57:35 +0530

    Ant generation

--- assets/scripts/AntGenerateManager.ts
+++ assets/scripts/AntGenerateManager.ts
@@ -10,7 +10,8 @@ export class AntGenerateManager extends Component {
     Ant=null;
     @property({type:Node})
     AddedAnt=null;
-    AntPool:NodePool;
+
+    AntPool:NodePool=new NodePool();
     private AntGenerateManager(){}
     static getInstance(): AntGenerateManager {
         if (!this._instance) {
@@ -28,16 +29,17 @@ export class AntGenerateManager extends Component {
         Shield:number)
       {
         
-      //  console.log(Ant.AntName);
+        console.log(AntName);
       console.log(this.AntPool.size())
-        //  if(this.AntPool.size()==0)
-        //  {
-        //         let fighetAnt=instantiate(this.Ant);
-        //         this.AntPool.put(fighetAnt);     
-        //  }
-        //  let newAnt=this.AntPool.get();
-        //  newAnt.getComponent(FighterAntScript).AddSpecs(AntName,TimeToCoverChangeInY,sprite,Health,Damage,CoinAlloted,Shield);
-        //  this.AddedAnt.addChild(newAnt);
+      console.log(this.Ant)
+         if(this.AntPool.size()==0)
+         {
+                let fighetAnt=instantiate(this.Ant);
+                this.AntPool.put(fighetAnt);     
+         }
+         let newAnt=this.AntPool.get();
+         newAnt.getComponent(FighterAntScript).AddSpecs(AntName,TimeToCoverChangeInY,sprite,Health,Damage,CoinAlloted,Shield);
+         this.AddedAnt.addChild(newAnt);
       }
     start() {
 
