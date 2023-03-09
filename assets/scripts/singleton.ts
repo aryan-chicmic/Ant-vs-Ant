c25af0e0cfb0371b4f0658b198581fecdf057263
Aryan <aryan.khanna@chicmic.co.in>
2023-03-09 23:07:10 +0530

    Removed parent.parent thing, compressed images,made hierarcy

--- assets/scripts/singleton.ts
+++ assets/scripts/singleton.ts
@@ -1,13 +1,22 @@
-import { _decorator, Component, Node, resources, AudioClip, Prefab, TiledMap } from "cc";
+import {
+  _decorator,
+  Component,
+  Node,
+  resources,
+  AudioClip,
+  Prefab,
+  TiledMap,
+} from "cc";
 const { ccclass, property } = _decorator;
 import { MAP_TYPES } from "./constants";
 @ccclass("singleton")
 export class singleton extends Component {
   private static instance: singleton = null;
-
-  parent_Node: Node = null;
-
-  AntPath: string;
+  static canvasNode: Node = null;
+  static PathDeciderNode: Node = null;
+  static antsHolder: Node = null;
+  static mapComponents: Node = null;
+  static AntPath: string;
   mapButton: string = "";
   static coins1 = 0;
   static coins2 = 0;
@@ -22,19 +31,40 @@ export class singleton extends Component {
   }
   get node_getter(): Node {
     console.log(" get Frame");
-    return this.parent_Node;
+    return singleton.PathDeciderNode;
   }
   set node_setter(value: Node) {
-    this.parent_Node = value;
+    singleton.PathDeciderNode = value;
+  }
+  get canvasNode_getter(): Node {
+    console.log(" get Frame");
+    return singleton.canvasNode;
+  }
+  set canvasNode_setter(value: Node) {
+    singleton.canvasNode = value;
+  }
+  get antsHolder_getter(): Node {
+    console.log(" get Frame");
+    return singleton.antsHolder;
+  }
+  set antsHolder_setter(value: Node) {
+    singleton.antsHolder = value;
+  }
+  get mapComponents_getter(): Node {
+    console.log(" get Frame");
+    return singleton.mapComponents;
+  }
+  set mapComponents_setter(value: Node) {
+    singleton.mapComponents = value;
   }
   mapAssigner(mapName: string) {
     this.mapButton = mapName;
   }
   setAntPath(path: string) {
-    this.AntPath = path;
+    singleton.AntPath = path;
   }
   getAntPath(): string {
-    return this.AntPath;
+    return singleton.AntPath;
   }
   start() {}
 
