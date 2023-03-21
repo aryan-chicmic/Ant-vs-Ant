2c18ca5ae0eb16394b4e3bc99e7fed3915892e8e
Aryan Khanna <aryan.khanna@chicmic.co.in>
2023-03-21 09:57:17 +0530

    updated popups

--- assets/scripts/ClassScripts/SettingPopUp.ts
+++ assets/scripts/ClassScripts/SettingPopUp.ts
@@ -10,42 +10,6 @@ export class SettingPopUp extends Component {
   start() {
     this.SingletonObject = singleton.getInstance();
   }
-  buttonClickedSoundEffect(ClipName: string) {
-    let audio = this.SingletonObject.getAudioFile(ClipName);
-    AudioControllerObject.playSoundEffetcs(audio);
-  }
-  /**
-   * Resume the Game After clicking Resume Button
-   */
-  resumeGame() {
-    // console.log("After Clicking Resume Button");
-    this.buttonClickedSoundEffect("buttonClickSound");
-    this.node.destroy();
-    director.resume();
-    // this.SettingPopUp.active = false;
-    this.AgainClickedSettingButton = false;
-  }
-  /**
-   * return to Landing Page
-   */
-  onClickMainMenu() {
-    this.buttonClickedSoundEffect("buttonClickSound");
-    this.node.destroy();
-    director.resume();
-    director.loadScene("MAIN");
-  }
-  /**
-   * Game Ends Quit Browser
-   */
-  quitGame() {
-    this.buttonClickedSoundEffect("buttonClickSound");
-    console.log("end");
-    game.end();
-  }
-  restartGame() {
-    this.node.destroy();
-    director.resume();
-    director.loadScene("MAP");
-  }
+
   update(deltaTime: number) {}
 }
--- assets/scripts/ClassScripts/functionalButtons.ts
+++ assets/scripts/ClassScripts/functionalButtons.ts
@@ -7,6 +7,8 @@ const { ccclass, property } = _decorator;
 export class menuButton extends Component {
   @property({ type: Prefab })
   SettingPopUp: Prefab = null;
+  @property({ type: Node })
+  audiosource = null;
   AgainClickedSettingButton: boolean = false;
   SingletonObject: singleton = null;
   settingPopUp: Node = null;
@@ -15,33 +17,33 @@ export class menuButton extends Component {
     this.SingletonObject = singleton.getInstance();
   }
 
-  buttonClickedSoundEffect(ClipName: string) {
-    let audio = this.SingletonObject.getAudioFile(ClipName);
-    AudioControllerObject.playSoundEffetcs(audio);
-  }
- 
+  // buttonClickedSoundEffect(ClipName: string) {
+  //   let audio = this.SingletonObject.getAudioFile(ClipName);
+  //   AudioControllerObject.playSoundEffetcs(audio);
+  // }
+
   /**
    * @description back to previous page from help pase
    *
    */
   onClickBackButton() {
-    this.buttonClickedSoundEffect("buttonClickSound");
+    // this.buttonClickedSoundEffect("buttonClickSound");
     console.log("Back Button Clicked");
     this.node.destroy();
     console.log("Help Page Destroyed");
   }
-  
+
   /**
    * Open Up Menu Option
    * Close the Menu Option if Open
    */
   menuButtonFunctionality() {
-    this.buttonClickedSoundEffect("buttonClickSound");
+    // this.buttonClickedSoundEffect("buttonClickSound");
     if (this.AgainClickedSettingButton == false) {
       this.AgainClickedSettingButton = true;
       this.settingPopUp = instantiate(this.SettingPopUp);
       this.settingPopUp.setPosition(100, 400);
-      this.node.addChild(this.settingPopUp);
+      this.SingletonObject.CanvasNode.addChild(this.settingPopUp);
       console.log(this.node);
 
       console.log("Game paused Menu Showed");
@@ -53,6 +55,40 @@ export class menuButton extends Component {
       console.log("Game Resumed");
       director.resume();
     }
+  }
+
+  /**
+   * Resume the Game After clicking Resume Button
+   */
+  resumeGame() {
+    // console.log("After Clicking Resume Button");
+    // this.buttonClickedSoundEffect("buttonClickSound");
+    director.resume();
+    this.node.destroy();
+    // this.SettingPopUp.active = false;
+    this.AgainClickedSettingButton = false;
+  }
+  /**
+   * return to Landing Page
+   */
+  onClickMainMenu() {
+    // this.buttonClickedSoundEffect("buttonClickSound");
+    this.node.destroy();
+    director.resume();
+    director.loadScene("MAIN");
+  }
+  /**
+   * Game Ends Quit Browser
+   */
+  quitGame() {
+    // this.buttonClickedSoundEffect("buttonClickSound");
+    console.log("end");
+    game.end();
+  }
+  restartGame() {
+    this.node.destroy();
+    director.resume();
+    director.loadScene("MAP");
   }
   update(deltaTime: number) {}
 }
